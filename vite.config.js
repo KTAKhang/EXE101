78e8b34b67579cca43e19e7c419cb18a9ba6f179
KTAKhang <khangkien.060603@gmail.com>
2025-07-17 19:23:16 +0700

    fix UI v3

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          ui: ['antd', '@material-tailwind/react', '@headlessui/react'],
-          redux: ['@reduxjs/toolkit', 'react-redux', 'redux-saga'],
-          icons: ['@heroicons/react', 'react-icons', 'lucide-react', 'remixicon'],
-          animation: ['framer-motion'],
-          charts: ['recharts']
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    host: true
+    sourcemap: false
   }
 })
