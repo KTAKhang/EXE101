2e8518403ad7fe9b92c719044e2ba84f79dbf48b
KTAKhang <khangkien.060603@gmail.com>
2025-07-18 08:58:51 +0700

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
+  resolve: {
+    alias: {
+      "@": "/src", // Đảm bảo rằng `@` trỏ tới thư mục src
+    },
   },
-  base: './' // Rất quan trọng để relative path hoạt động đúng trên Vercel
 });
